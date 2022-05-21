7e777eb6ff9570dec80678a7292bc778974a226f
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-05-22 00:53:19 +0900

    chapter1_4/json

--- Paramiko/chapter1_4.py
+++ Paramiko/chapter1_4.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python
 
-from multiprocessing import connection
 import paramiko, getpass, time, json
 
 with open('devices.json', 'r') as f:
     devices = json.load(f)
 
-with open('commands.txt', 'r') as f:
+with open('commands.txt', 'r') as f: 
     commands = f.readlines()
 
 username = input('Username: ')
@@ -18,21 +17,23 @@ def clear_buffer(connection):
     if connection.recv_ready():
         return connection.recv(max_buffer)
 
-for device in devices.keys():
-    outputFilename = device + '_output.txt'
+# Starts the loop for devices
+for device in devices.keys(): 
+    outputFileName = device + '_output.txt'
     connection = paramiko.SSHClient()
     connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    connection.connect(devices[device]['ip'], username=username, password=password, look_for_keys=False, allow_agent=False)    
+    connection.connect(devices[device]['ip'], username=username, password=password, look_for_keys=False, allow_agent=False)
     new_connection = connection.invoke_shell()
     output = clear_buffer(new_connection)
-    time.sleep(5)
+    time.sleep(2)
     new_connection.send("terminal length 0\n")
     output = clear_buffer(new_connection)
-    with open(outputFilename, 'wb') as f:
+    with open(outputFileName, 'wb') as f:
         for command in commands:
             new_connection.send(command)
-            time.sleep(5)
+            time.sleep(2)
             output = new_connection.recv(max_buffer)
             print(output)
             f.write(output)
-    new_connection.close()        
+    
+    new_connection.close()
